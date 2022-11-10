17c7c15479fcbf0603ea40e100f8a50a4802590a
Harsh-Kumar-Sharma <gaurharsh1737@gmail.com>
2022-11-10 17:24:38 +0530

    this assignment axoius type

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const CowinController= require("../controllers/cowinController")
+const memeController= require("../controllers/memeController")
+const cloudeController= require("../controllers/cloudeController")
 
 
 
@@ -15,8 +17,12 @@ router.get("/cowin/getByPin", CowinController.getByPin)
 
 router.post("/cowin/getOtp", CowinController.getOtp)
 
+router.get("/cowin/getdistidvac", CowinController.getdistidvac)
 // WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
 
+router.get("/meme/get_memes", memeController.get_memes)
+router.post("/meme/creatememe", memeController.creatememe)
 
+router.get("/getcloudedetails", cloudeController.getcloudedetails)
 
 module.exports = router;
